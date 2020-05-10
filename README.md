a962750b70dcd649698d2d752382c0b6bb9f356c
hx24 <83036406@qq.com>
2020-05-10 21:56:22 +0800

    增加是否启用配置

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-## 将普通函数转为箭头函数的babel插件
+## 将普通函数转为箭头函数的 babel 插件
+
 安装
+
 ```
 yarn add babel-plugin-nomarlfn-to-arrowfn -D
 ```
-配置.babelrc使用插件
+
+配置.babelrc 使用插件
+
 ```
 {
   "plugins": [
-    "nomarlfn-to-arrowfn"
+    [
+      "nomarlfn-to-arrowfn",
+      {
+        "enable": true  // 是否启用，默认为true
+      }
+    ]
   ]
 }
+
 ```
--- index.js
+++ index.js
@@ -5,7 +5,10 @@ module.exports = function (babel) {
     name: "nomarlfn-to-arrowfn", // not required
     visitor: {
       FunctionDeclaration(path, state) {
-        console.log('state', state)
+        console.log('opts', state.opts)  // 配置
+        if (state.opts.enable === false) {
+          return
+        }
         const id = path.node.id
         const arrowFunction = t.arrowFunctionExpression(path.node.params, path.node.body)
         path.replaceWith(t.variableDeclaration('const', [t.variableDeclarator(id, arrowFunction)]))
